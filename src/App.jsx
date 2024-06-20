bc30f0a4d3b300f87c7bff889f629d632233aa87
kidus Tesfaye <kidus.z3rd@gmail.com>
2024-06-20 21:45:52 +0300

    update: numbers section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./common_components/navbar";
 import Hero from "./homepage_components/hero";
 import ImageList from "./homepage_components/image_list";
 import MayorInfo from "./homepage_components/mayor_info";
+import Numbers from "./homepage_components/numbers";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <MayorInfo />
       <ImageList />
+      <Numbers />
     </div>
   );
 }
---
+++ src/homepage_components/numbers.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function Numbers() {
+  return (
+    <div>
+      <div className="bg-blue-900 w-full my-3">
+        <div className="flex justify-between px-5  text-gray-200 text-xl">
+          <div className="m-3 p-6 shadow-2xl border-l border-gray-700 rounded-xl  my-9">
+            <h1 className="text-center font-semibold">5000</h1>
+            <h1 className="text-gray-400 font-semibold">
+              Total People Living In Mechara City
+            </h1>
+          </div>
+          <div className="m-3 p-6 shadow-2xl border-l border-gray-700 rounded-xl  my-9">
+            <h1 className="text-center font-semibold">3000</h1>
+            <h1 className="text-gray-400 font-semibold">Services We Provide</h1>
+          </div>
+          <div className="m-3 p-6 shadow-2xl border-l border-gray-700 rounded-xl  my-9">
+            <h1 className="text-center font-semibold">5000</h1>
+            <h1 className="text-gray-400 font-semibold">
+              Reaches on Soical Media
+            </h1>
+          </div>
+          <div className="m-3 p-6 shadow-2xl border-l border-gray-700 rounded-xl  my-9">
+            <h1 className="text-center font-semibold">5000</h1>
+            <h1 className="text-gray-400 font-semibold">
+              Total number of Projecs
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
